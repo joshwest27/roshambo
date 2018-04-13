89fcdbd1a9737218e21275b056a1584f740864ed
Josh West <joshw@zillowgroup.com>
2018-04-13 14:14:43 -0700

    finished setup of files

--- js/app.js
+++ js/app.js
@@ -1,4 +1,13 @@
 // This is a simple Rock, Paper, Scissors game.
-'use strict!'
+'use strict!';
+
+
+// Login page
+const welcomeUser = (userName) => {
+  console.log(`Hello ${userName}. Let's play some Rock, Paper, Scissors!`);
+};
+
+welcomeUser();
+
 
 

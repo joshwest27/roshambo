29cbb395872cb4b415df9759c39492b8739766d4
Josh West <joshw@zillowgroup.com>
2018-05-09 14:51:06 -0700

    sound and mute button on about me page

--- js/app.js
+++ js/app.js
@@ -1,14 +1,6 @@
 // This is a simple Rock, Paper, Scissors game.
 'use strict!';
 
-
-// Login page
-// const welcomeUser = (userName) => {
-//   console.log(`Hello ${userName}. Let's play some Rock, Paper, Scissors!`);
-// };
-
-// welcomeUser();
-
 // Toggle sound on and off
 $('#muteIcon').hide();
 
